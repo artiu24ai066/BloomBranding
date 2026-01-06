d85e5b0cbdee998714b8708e42a878b5577117c7
rimjhim010 <rimjhim.gondane@gmail.com>
2026-01-07 01:17:17 +0530

    littile change in home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,14 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Services from "./components/Services";
-import Work from "./components/Work";
-import Journey from "./components/Journey";
-import WallOfLove from "./components/WallOfLove";
-import MenuOverlay from "./components/MenuOverlay";
+import Home from "./pages/Home";
 import { useState } from "react";
-import MenuButton from "./components/MenuButton";
 import "./App.css";
 
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
-
-    <div className="app">
-      <MenuButton onClick={() => setMenuOpen(true)} />
-
-      {menuOpen && <MenuOverlay onClose={() => setMenuOpen(false)} />}
-
-      <Navbar />
-      <Hero />
-      <Services />
-      <Work />
-      <Journey />
-      <WallOfLove />
-    </div>
+    <>
+    <Home />
+    </>
   );
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,12 @@
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Services from "../components/Services";
+import Work from "../components/Work";
+import Journey from "../components/Journey";
+import WallOfLove from "../components/WallOfLove";
+import MenuOverlay from "../components/MenuOverlay";
+import { useState } from "react";
+import MenuButton from "../components/MenuButton";
 const Home = () => {
   return (
     <main style={{ padding: "80px 40px" }}>
@@ -5,6 +14,18 @@ const Home = () => {
       <p>
         We craft bold brand identities and immersive digital experiences.
       </p>
+      <div className="app">
+      <MenuButton onClick={() => setMenuOpen(true)} />
+
+      {menuOpen && <MenuOverlay onClose={() => setMenuOpen(false)} />}
+
+      <Navbar />
+      <Hero />
+      <Services />
+      <Work />
+      <Journey />
+      <WallOfLove />
+    </div>
     </main>
   );
 };
